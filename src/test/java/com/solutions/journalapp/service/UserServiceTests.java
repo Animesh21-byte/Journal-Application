e82791f4fe7fa22725cfa247630802598a3c9685
Animesh21-byte <aranimesh926@gmail.com>
2026-05-30 20:37:48 +0530

    Unit Testing using JUnit

---
+++ src/test/java/com/solutions/journalapp/service/UserServiceTests.java
@@ -0,0 +1,44 @@
+package com.solutions.journalapp.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.solutions.journalapp.repository.UserRepository;
+
+@SpringBootTest
+public class UserServiceTests{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Disabled
+    @Test
+    public void JournalPresentTest(){
+        assertTrue(userRepository.findByUserName("Ani").getJournalEntries().isEmpty());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1,2,3",
+        "4,6,10",
+        "20,10,30"
+    })
+    public void testAdd(int a,int b,int expected){
+        assertEquals(expected, a+b);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "Ani",
+        "Homelander"
+    })
+    public void userPresentTest(String name){
+        assertNotNull(userRepository.findByUserName(name),"Failed for :"+name);
+    }
+}
